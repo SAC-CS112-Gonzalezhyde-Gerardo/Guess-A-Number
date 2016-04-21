853156b410d833244b821b9d64142a75eef693f1
Gerry Gonzalez <Gerry Gonzalez@DESKTOP-N2EM1CQ>
2016-04-20 19:43:51 -0700

    commit 1

---
+++ src/RandomNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RandomNumber {
+
+static Scanner input = new Scanner (System.in);
+		public static void main(String args[]){
+			RandomNumber myRandomNumber = new RandomNumber();
+			
+			System.out.println("A random number between 1 and 10 will be generated");
+			int x = myRandomNumber.GetANumber();
+			System.out.println("x");
+		
+		
+	}
+		private int GetANumber() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+}
